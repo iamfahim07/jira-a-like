858c9c1c7f4850eee780f9e3ecf027b9ec6a6bf4
Fahim <mfrfahim07@gmail.com>
2024-12-02 14:09:09 +0600

    Added workspace update functionality

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { SettingsIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   GoCheckCircle,
   GoCheckCircleFill,
   GoHome,
   GoHomeFill,
 } from "react-icons/go";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 import { cn } from "@/lib/utils";
 
@@ -37,14 +42,18 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const WorkspaceId = useWorkspaceId();
+  const pathname = usePathname();
+
   return (
     <ul className="flex flex-col">
       {routes.map((item) => {
-        const isActive = false;
+        const fullHref = `/workspaces/${WorkspaceId}${item.href}`;
+        const isActive = pathname === fullHref;
         const Icon = isActive ? item.activeIcon : item.icon;
 
         return (
-          <Link key={item.href} href={item.href}>
+          <Link key={item.href} href={fullHref}>
             <div
               className={cn(
                 "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
